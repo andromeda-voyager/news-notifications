f77e7e7547b26f590bcb49f3847a42a36bfd2e57
Matthew Potts <dev.matthewpotts@gmail.com>
2021-06-03 06:41:01 -0600

    Changed layout of notifications

--- notify/notify.py
+++ notify/notify.py
@@ -1,15 +1,15 @@
 import sys
 from PySide6 import QtCore, QtWidgets, QtGui
-from PySide6.QtGui import QAction, QIcon
-from PySide6.QtWidgets import QApplication, QLabel, QMenu
-import time
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QApplication
 import guardian
 
 
-class SystemTray(QtWidgets.QWidget):
+class SystemTrayItem(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.tray_icon = QtWidgets.QSystemTrayIcon(QtGui.QIcon("icon.png"), self)
+        self.tray_icon = QtWidgets.QSystemTrayIcon(
+            QtGui.QIcon("icon.png"), self)
         self.menu = QtWidgets.QMenu(self)
         self._quit_action = QAction("Quit", self)
         self._quit_action.triggered.connect(QApplication.quit)
@@ -17,55 +17,75 @@ class SystemTray(QtWidgets.QWidget):
         self.tray_icon.setContextMenu(self.menu)
         self.tray_icon.show()
         self.widget = Notification()
-        geo = QApplication.primaryScreen().geometry()
-        height = geo.height()
-        width = geo.width()
-        self.widget.setGeometry(width * 0.95, 0.025 * height, 100, 100)
 
 
 class Notification(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-        self.button = QtWidgets.QPushButton("x")
-        self.button.clicked.connect(self.dismiss)
-        self.button.resize(10, 10)
-        self.text = QtWidgets.QLabel("", alignment=QtCore.Qt.AlignCenter)
+        self.setStyleSheet("background-color: #343638;")
+        self.setFixedSize(500, 100)
+        geo = QApplication.primaryScreen().geometry()
+        width = geo.width()
+        self.setGeometry(width-self.width()-width*.005,
+                         40, 500, 100)
+
+        self.button_close = QtWidgets.QPushButton("X", self)
+        self.button_close.clicked.connect(self.dismiss)
+        self.button_close.setStyleSheet(
+            "QPushButton:hover{font-weight: bold} QPushButton{border:none; padding:8px; color:white;}"
+        )
+
+        self.title = QtWidgets.QLabel("", self)
+        self.title.setOpenExternalLinks(True)
+        self.title.setStyleSheet("padding:10px")
+
+        self.button_next = QtWidgets.QPushButton("next", self)
+        self.button_next.setStyleSheet(
+            "QPushButton:hover{font-weight: bold} QPushButton{border:none; padding:10px; color:white;}"
+        )
+
+        self.button_next.clicked.connect(self.next)
+
+        self.top_layout = QtWidgets.QHBoxLayout()
+        self.top_layout.addWidget(
+            self.button_next)
+        self.top_layout.addStretch()
+        self.top_layout.addWidget(self.button_close)
+
         self.layout = QtWidgets.QVBoxLayout(self)
-        layout_top = QtWidgets.QHBoxLayout()
-        layout_top.addWidget(self.button, QtCore.Qt.AlignRight)
-        self.layout.addLayout(layout_top)
-        self.layout.addWidget(self.text)
-        self.button.setFixedSize(25, 25)
-        self.setStyleSheet("color: white; background-color: #343638;")
-        self.articles = []
-        self.next_button = QtWidgets.QPushButton("next")
-        self.layout.addWidget(self.next_button)
-        self.next_button.clicked.connect(self.next)
-        QtCore.QTimer.singleShot(5000, self.notify)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.addLayout(self.top_layout)
+        self.layout.addWidget(self.title)
+
+        QtCore.QTimer.singleShot(1000, self.notify)
 
     def next(self):
         self.articles.pop()
-        self.text.setText(self.articles[len(self.articles) - 1].get_title())
+        article = self.articles[len(self.articles)-1]
+        self.title.setText("<a style=\"color:white;\" href=" + article.get_link() +
+                           ">" + article.get_title() + "</a>")
         if len(self.articles) < 2:
-            self.next_button.hide()
+            self.button_next.hide()
 
     def notify(self):
         articles = guardian.check_for_new()
         self.articles = articles
         if len(articles) > 0:
-            self.text.setText(articles[len(articles) - 1].get_title())
+            article = articles[len(articles)-1]
+            self.title.setText("<a style=\"color:white;\" href=" + article.get_link() +
+                               ">" + article.get_title() + "</a>")
             self.show()
 
     def dismiss(self):
-        QtCore.QTimer.singleShot(5000, self.notify)
+        QtCore.QTimer.singleShot(1000, self.notify)
         self.hide()
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     if QtWidgets.QSystemTrayIcon.isSystemTrayAvailable():
-        tray = SystemTray()
+        tray = SystemTrayItem()
         sys.exit(app.exec())
 
 
